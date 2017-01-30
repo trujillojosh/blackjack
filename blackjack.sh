f39a04c6edf41b9350f0cbbf6d968486b9032a7c
trujillojosh <trujilloj@me.com>
2017-01-30 13:52:55 -0800

    added shell and small loan

---
+++ blackjack.sh
@@ -0,0 +1,3 @@
+make re
+clear
+./blackjack
---
+++ small_loan.c
@@ -0,0 +1,13 @@
+int	small_loan(void)
+{
+	int	res;
+	char	*ans;
+	
+	ans = 
+	res = 0;
+	system("clear");
+	printf("\n%s\n", "Hello, I'm Donald Trump, the greatest person of all time. I mean come on, I'm pretty great.\n
+			I'm looking to invest in a new business venture, Trump University 2. We can charge all of these \n
+			losers tons of money and hire other losers to teach them. It's gonna be a Yuge success! \n
+			To get started, all I need is a small loan of a million dollars.");
+	 

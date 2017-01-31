df7c0416d7a4730896c63b640968d77c8e15d3d2
trujillojosh <trujilloj@me.com>
2017-01-30 22:40:33 -0800

    added linux compatibility

--- blackjack.sh
+++ blackjack.sh
@@ -1,12 +1,20 @@
 clear
 cd src > /dev/null
-make re > /dev/null
-make clean > /dev/null
 cat intro.txt
-read -n1 -s input
+read input
 if [ "$input" = "1" ] ; then
 	clear
 	cat credits.txt
+elif [ "$input" = "2" ] ; then
+	echo "\nFixing...you only need to run this the first time you play\n"
+	mv get_card.c old_get_card.c
+	cp linux_card.c get_card.c
+	make fclean > /dev/null
+	make re > /dev/null
+	make clean > /dev/null
+	./blackjack
 else
+	make re > /dev/null
+	make clean > /dev/null
 	./blackjack
 fi
